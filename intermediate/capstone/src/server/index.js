0e75d0ce8cff47c6d26009590efae1360d69f1de
TODD FREDERKING <toddfrederking@gmail.com>
2018-06-27 19:10:04 -0500

    at least its working

--- intermediate/capstone/src/server/index.js
+++ intermediate/capstone/src/server/index.js
@@ -1,25 +1,22 @@
-import app from "../app.js";
-
-let express = require("express");
+const express = require("express");
 const mongoose = require ("mongoose");
-
-
-const tshirtRoutes = ("./Routes/tshirtroutes");
-
-
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://toddfrederking:Lgarrett1970@ds163680.mlab.com:63680/capstone0618");
 console.log("connection successful");
 
+const app = express();
+ 
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
 
+const tshirtRoutes = require ("./Routes/tshirtroutes");
 
+app.use(tshirtRoutes);
 
-app.use(express.static("public"));
-// app.use(tshirtRoutes);
+app.listen(3001, (err) => {
+  if (err) {
+    return console.log("Error", err);
+  }
+  console.log("Web server is now living in apartment 3001(really)");
+  });
 
-app.listen(3002, (err) => {
-if (err) {
-  return console.log("Error", err);
-}
-console.log("Web server is now living in apartment 3002(really)");
-});
